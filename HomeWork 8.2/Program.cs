64311b40825af57beba38af85f6095b51364127c
ИК <@DESKTOP-IVMPSKR>
2022-07-26 00:15:08 +0300

    Добавьте файлы проекта.

---
+++ HomeWork 8.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> PhoneNumbers = new Dictionary<string, string>();
+            Console.WriteLine("введите номер телефона и фио\nввод пустой строки останавливает запись");
+            string[] arr = new string[3];
+            while(true)
+            {
+                arr = Console.ReadLine().Split(' ');
+                if (arr[0] != "")
+                {
+                    PhoneNumbers.Add(arr[0], $"{arr[1]} {arr[2]}");
+                }
+                else break;
+            }
+            Console.WriteLine("чей телефон хотите найти?");
+            string number = Console.ReadLine();
+            if (PhoneNumbers.TryGetValue(number, out string value))
+            {
+                Console.WriteLine($"телефоном {number} владеет {value}");
+            }
+            else Console.WriteLine("такого телефона в базе нет");
+            Console.ReadKey();
+        }
+    }
+}
